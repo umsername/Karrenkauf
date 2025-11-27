ed7efcee23324c0f68b68f8d311c96d383abe73c
a <PScholtz24@edu.asw-berufsakademie.de>
2025-11-27 15:50:38 +0100

    added unit fuction

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,40 @@
 <script setup>
 import { ref } from 'vue'
+import units from '../units.json'
 
 defineProps({
   msg: String,
 })
 
+const selectedUnit = ref('')
 const count = ref(0)
 </script>
 
 <template>
+  <h1>Karrenkauf</h1>
+  <p> Die morderne Art Einkaufslisten zu gestalten, ohne Risiko und alles lokal!</p>
+  <img src="/src/assets/karrenkauf.jfif" alt="KARRENKAUF" width="200" height="200"></img>
+
+  <div class="">
+    <form action="">
+      <input type="text" id="newList" name="newList" placeholder="Name der neuen Einkaufsliste" size="30"></input>
+      <input type="submit" value="Erstellen"></input>
+    </form>
+  </div>
+
+    <div class="">
+    <form action="">
+      <input type="text" id="newItem" name="newItem" placeholder="Neues Produkt" size="30"/>
+        <select v-model="selectedUnit">
+          <option disabled value="">Einheit wählen...</option>
+          <option v-for="unit in units.units" :key="unit.value" :value="unit.value">
+            {{ unit.label }}
+          </option>
+        </select>
+      <input type="submit" value="Hinzufügen"/>
+    </form>
+  </div>
+
   <h1>{{ msg }}</h1>
 
   <div class="card">
